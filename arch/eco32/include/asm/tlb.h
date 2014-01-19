b30579ca1c2a14e2c9793d90410b2a7c8ad57acb
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-01-19 05:32:20 +0200

    eco32: implement flush_xxx routines
    Signed-off-by: Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>

--- arch/eco32/include/asm/tlb.h
+++ arch/eco32/include/asm/tlb.h
@@ -9,6 +9,31 @@
 #ifndef __ASM_ECO32_TLB_H
 #define __ASM_ECO32_TLB_H
 
+#define NUM_TLB_ENTRIES	32
+/*
+ * Writes out the value in TLB_ENTRY_HIGH and TLB_ENTRY_LOW to the tlb
+ */
+static inline void tbwi(void)
+{
+	__asm__ __volatile__("tbwi");
+}
+
+/*
+ * Reads the value in the tlb to TLB_ENTRY_HIGH and TLB_ENTRY_LOW
+ */
+static inline void tbri(void)
+{
+	__asm__ __volatile__("tbri");
+}
+
+/*
+ * Search the tlb for a virtual address
+ */
+static inline void tbs(void)
+{
+	__asm__ __volatile__("tbs");
+}
+
 #define tlb_start_vma(tlb, vma)				do { } while (0)
 #define tlb_end_vma(tlb, vma)				do { } while (0)
 #define __tlb_remove_tlb_entry(tlb, ptep, address)	do { } while (0)
--- arch/eco32/include/asm/tlbflush.h
+++ arch/eco32/include/asm/tlbflush.h
@@ -14,6 +14,8 @@
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <asm/current.h>
+#include <asm/spr.h>
+#include <asm/tlb.h>
 #include <linux/sched.h>
 
 /*
@@ -23,28 +25,61 @@
  *  - flush_tlb_page(vma, vmaddr) flushes one page
  *  - flush_tlb_range(mm, start, end) flushes a range of pages
  */
-
 static inline void flush_tlb_all(void)
 {
-	BUG(); /* SJK TODO */
+	int i;
+
+	mvts(SPR_TLB_ENTRY_HIGH, 0);
+	mvts(SPR_TLB_ENTRY_LOW, 0);
+	for (i = 0; i < NUM_TLB_ENTRIES; i++) {
+		mvts(SPR_TLB_INDEX, i);
+		tbwi();
+	}
 }
 
 static inline void flush_tlb_mm(struct mm_struct *mm)
 {
-	BUG(); /* SJK TODO */
+	/* eco32 only have 32 tlbs, so let's just flush them all */
+	flush_tlb_all();
 }
 
 static inline void flush_tlb_page(struct vm_area_struct *vma,
 				  unsigned long addr)
 {
-	BUG(); /* SJK TODO */
+	unsigned long tlb_index;
+
+	/*
+	 * The tbs instruction search the tlbs for the virtual address
+	 * present in SPR_TLB_ENTRY_HIGH. If found, the tlb index is returned
+	 * in SPR_TLB_INDEX otherwise SPR_TLB_INDEX contains 0x80000000.
+	 */
+	mvts(SPR_TLB_ENTRY_HIGH, addr);
+	tbs();
+	tlb_index = mvfs(SPR_TLB_INDEX);
+
+	if (tlb_index != 0x80000000) {
+		mvts(SPR_TLB_ENTRY_HIGH, 0);
+		mvts(SPR_TLB_ENTRY_LOW, 0);
+		tbwi();
+	}
 }
 
 static inline void flush_tlb_range(struct vm_area_struct *vma,
 				   unsigned long start,
 				   unsigned long end)
 {
-	BUG(); /* SJK TODO */
+	int i;
+	unsigned long addr;
+
+	for (i = 0; i < NUM_TLB_ENTRIES; i++) {
+		mvts(SPR_TLB_INDEX, i);
+		addr = mvfs(SPR_TLB_ENTRY_HIGH);
+		if (addr >= start && addr < end) {
+			mvts(SPR_TLB_ENTRY_HIGH, 0);
+			mvts(SPR_TLB_ENTRY_LOW, 0);
+			tbwi();
+		}
+	}
 }
 
 static inline void flush_tlb(void)
