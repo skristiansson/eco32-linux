aa18e5bc3473ca7a47db0ed60ef4c6f224e502af
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-02-16 17:28:47 +0200

    eco32: fix delay implementation
    This implements __delay as a (tight) loop.
    This means that we will need to rely on the "heavy-weight"
    calibration to obtain loops_per_jiffy.
    
    Signed-off-by: Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>

--- arch/eco32/lib/delay.c
+++ arch/eco32/lib/delay.c
@@ -1,4 +1,14 @@
-/* SJK FIXME */
+/*
+ * Copyright (C) 2014 Stefan Kristiansson, stefan.kristiansson@saunalahti.fi
+ *
+ * Based on the OpenRISC implementation:
+ * Copyright (C) 2010-2011 Jonas Bonn <jonas@southpole.se>
+ *
+ * This file is licensed under the terms of the GNU General Public License
+ * version 2, or (at your option) any later version.
+ * This program is licensed "as is" without any warranty of any
+ * kind, whether express or implied.
+ */
 
 #include <linux/delay.h>
 #include <linux/kernel.h>
@@ -10,30 +20,18 @@
 
 extern unsigned long loops_per_jiffy;
 
-void __delay(unsigned long cycles)
+void __delay(unsigned long loops)
 {
-	cycles_t start = get_cycles();
-
-	/* SJK FIXME */
-	while (cycles--)
-		cpu_relax();
-	return;
-	/* SJK FIXME */
-
-	while ((get_cycles() - start) < cycles)
-		cpu_relax();
+	__asm__ __volatile__("1:\n\t"			\
+			     "subi\t%0,%0,1\n\t"	\
+			     "bgt\t%0,$0,1b\n"		\
+			     : : "r" (loops));
 }
 EXPORT_SYMBOL(__delay);
 
 inline void __const_udelay(unsigned long xloops)
 {
 	unsigned long long loops;
-
-	/* SJK FIXME */
-	loops = (unsigned long long)xloops * 50 * HZ;
-	__delay(loops >> 32);
-	return;
-	/* SJK FIXME */
 
 	loops = (unsigned long long)xloops * loops_per_jiffy * HZ;
 
