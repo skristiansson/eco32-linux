35ef9d71a3dbdbe78feb3650eaad30066ec5099e
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-02-19 03:41:05 +0200

    eco32: add dma support
    This should be considered as a stub implementation since there
    are no cache architecture defined for eco32 yet.
    
    Signed-off-by: Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>

--- arch/eco32/kernel/dma.c
+++ arch/eco32/kernel/dma.c
@@ -1,9 +1,19 @@
 /*
  * Copyright (C) 2014 Stefan Kristiansson, stefan.kristiansson@saunalahti.fi
  *
+ * Based on the OpenRISC implementation:
+ * Copyright (C) 2003 Matjaz Breskvar <phoenix@bsemi.com>
+ * Copyright (C) 2010-2011 Jonas Bonn <jonas@southpole.se>
+ *
  * This file is licensed under the terms of the GNU General Public License
  * version 2.  This program is licensed "as is" without any warranty of any
  * kind, whether express or implied.
+ */
+
+/*
+ * NOTE: this code is completely untested on eco32, at the time of writing this,
+ * there are no devices using DMA and there is no cache architecture defined for
+ * eco32 yet.
  */
 
 #include <linux/dma-mapping.h>
@@ -17,7 +27,13 @@ static int
 page_set_nocache(pte_t *pte, unsigned long addr,
 		 unsigned long next, struct mm_walk *walk)
 {
-	BUG(); /* SJK TODO */
+	pte_val(*pte) |= _PAGE_NONCACHED;
+
+	/*
+	 * Flush the page out of the TLB so that the new page flags get
+	 * picked up next time there's an access
+	 */
+	flush_tlb_page(NULL, addr);
 
 	return 0;
 }
@@ -26,7 +42,13 @@ static int
 page_clear_nocache(pte_t *pte, unsigned long addr,
 		   unsigned long next, struct mm_walk *walk)
 {
-	BUG(); /* SJK TODO */
+	pte_val(*pte) &= ~_PAGE_NONCACHED;
+
+	/*
+	 * Flush the page out of the TLB so that the new page flags get
+	 * picked up next time there's an access
+	 */
+	flush_tlb_page(NULL, addr);
 
 	return 0;
 }
@@ -36,18 +58,62 @@ eco32_dma_alloc(struct device *dev, size_t size,
 		dma_addr_t *dma_handle, gfp_t gfp,
 		struct dma_attrs *attrs)
 {
-	BUG(); /* SJK TODO */
+	unsigned long va;
+	void *page;
+	struct mm_walk walk = {
+		.pte_entry = page_set_nocache,
+		.mm = &init_mm
+	};
 
-	return 0;
+	page = alloc_pages_exact(size, gfp);
+	if (!page)
+		return NULL;
+
+	/* This gives us the real physical address of the first page. */
+	*dma_handle = __pa(page);
+
+	va = (unsigned long)page;
+
+	if (!dma_get_attr(DMA_ATTR_NON_CONSISTENT, attrs)) {
+		/*
+		 * We need to iterate through the pages, clearing the dcache for
+		 * them and setting the cache-inhibit bit.
+		 */
+		if (walk_page_range(va, va + size, &walk)) {
+			free_pages_exact(page, size);
+			return NULL;
+		}
+	}
+
+	return (void *)va;
 }
 
 static void
 eco32_dma_free(struct device *dev, size_t size, void *vaddr,
 	       dma_addr_t dma_handle, struct dma_attrs *attrs)
 {
-	BUG(); /* SJK TODO */
+	unsigned long va = (unsigned long)vaddr;
+	struct mm_walk walk = {
+		.pte_entry = page_clear_nocache,
+		.mm = &init_mm
+	};
 
-	return 0;
+	if (!dma_get_attr(DMA_ATTR_NON_CONSISTENT, attrs)) {
+		/* walk_page_range shouldn't be able to fail here */
+		WARN_ON(walk_page_range(va, va + size, &walk));
+	}
+
+	free_pages_exact(vaddr, size);
+}
+
+static void flush_dcache(unsigned start, size_t size)
+{
+	/* TODO */
+}
+
+static void invalidate_dcache(unsigned start, size_t size)
+{
+	/* TODO */
 }
 
 static dma_addr_t
@@ -56,9 +122,26 @@ eco32_map_page(struct device *dev, struct page *page,
 	       enum dma_data_direction dir,
 	       struct dma_attrs *attrs)
 {
-	BUG(); /* SJK TODO */
+	unsigned long cl;
+	dma_addr_t addr = page_to_phys(page) + offset;
 
-	return 0;
+	switch (dir) {
+	case DMA_TO_DEVICE:
+		flush_dcache(addr, size);
+		break;
+	case DMA_FROM_DEVICE:
+		invalidate_dcache(addr, size);
+		break;
+	default:
+		/*
+		 * NOTE: If dir == DMA_BIDIRECTIONAL then there's no need to
+		 * flush nor invalidate the cache here as the area will need
+		 * to be manually synced anyway.
+		 */
+		break;
+	}
+
+	return addr;
 }
 
 static void
@@ -73,9 +156,15 @@ eco32_map_sg(struct device *dev, struct scatterlist *sg,
 	     int nents, enum dma_data_direction dir,
 	     struct dma_attrs *attrs)
 {
-	BUG(); /* SJK DEBUG */
+	struct scatterlist *s;
+	int i;
 
-	return 0;
+	for_each_sg(sg, s, nents, i) {
+		s->dma_address = eco32_map_page(dev, sg_page(s), s->offset,
+						s->length, dir, NULL);
+	}
+
+	return nents;
 }
 
 static void
@@ -83,7 +172,12 @@ eco32_unmap_sg(struct device *dev, struct scatterlist *sg,
 	      int nents, enum dma_data_direction dir,
 	      struct dma_attrs *attrs)
 {
-	BUG(); /* SJK DEBUG */
+	struct scatterlist *s;
+	int i;
+
+	for_each_sg(sg, s, nents, i) {
+		eco32_unmap_page(dev, sg_dma_address(s), sg_dma_len(s), dir, NULL);
+	}
 }
 
 static void
@@ -91,7 +185,7 @@ eco32_sync_single_for_cpu(struct device *dev,
 			  dma_addr_t dma_handle, size_t size,
 			  enum dma_data_direction dir)
 {
-	BUG(); /* SJK TODO */
+	invalidate_dcache(dma_handle, size);
 }
 
 static void
@@ -99,7 +193,7 @@ eco32_sync_single_for_device(struct device *dev,
 			     dma_addr_t dma_handle, size_t size,
 			     enum dma_data_direction dir)
 {
-	BUG(); /* SJK TODO */
+	flush_dcache(dma_handle, size);
 }
 
 struct dma_map_ops eco32_dma_map_ops = {
