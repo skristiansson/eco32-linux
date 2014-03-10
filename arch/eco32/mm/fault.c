5a59ecf0e329d7aadaf9d95a4a4d44933e41e59f
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-03-10 20:48:49 +0200

    eco32: fault: send sigsegv when hitting a bad area in user mode
    An user screw up shouldn't be able to crash the kernel.
    
    Signed-off-by: Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>

--- arch/eco32/mm/fault.c
+++ arch/eco32/mm/fault.c
@@ -148,6 +148,15 @@ good_area:
 bad_area:
 	up_read(&mm->mmap_sem);
 
+	if (user_mode(regs)) {
+		info.si_signo = SIGSEGV;
+		info.si_errno = 0;
+		info.si_code = SEGV_ACCERR;
+		info.si_addr = (void __user *)address;
+		force_sig_info(SIGBUS, &info, current);
+		return;
+	}
+
 no_context:
 	/*
 	 * Are we prepared to handle this kernel fault?
